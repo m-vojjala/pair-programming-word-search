8c599272b3757b17f7d74d68f7f76580912ae0d1
MounikaVojjala <mounikavojjala@gmail.com>
2020-11-04 22:32:44 +0000

    pair programming word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
-module.exports = wordSearch
+  // const horizontalJoin = letters.map(ls => ls.join(''))
+  // for (l of horizontalJoin) {
+  //     if (l.includes(word)) return true
+  // }
+
+    
+  const wordSearch = (letters, word) => {
+    let row = "";
+    let column = "";
+    for (let i = 0; i < letters[0].length; i++) {
+      for (let j = 0; j < letters[i].length; j++) {
+        row += letters[i][j];
+        column += letters[j][i];
+        if (row.includes(word)) {
+          return true;
+        } else if (column.includes(word)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+
+  module.exports = wordSearch;
